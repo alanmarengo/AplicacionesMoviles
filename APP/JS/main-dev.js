08574d0b9d3eab1cf7d483e5122901a8710aed0e
alanmarengo98 <alanmarengo98@gmail.com>
2023-09-09 20:53:31 -0300

    AP-13 <Avances en header mobile>

---
+++ APP/JS/main-dev.js
@@ -0,0 +1,40 @@
+$(function() {
+  $(".imagenBuscador").on('click', function(){
+    //recordar que aqui se debe preguntar si es != de resolucion mobile
+    
+    $(".imagenCarrito").css("display","none");
+    $(".imagenUsuario").css("display","none");
+    $(".imagenHamburguesa").css("display","none");
+    $(".imagenBuscador").css("display","none");
+    $(".logo").css("display","none");
+    $(".BuscadorDiv").css("display","flex");
+    $(".mainContainer").css("filter","brightness(50%)");
+  });
+
+  $(".flechabuscador").on('click', function(){  
+    $(".imagenCarrito").css("display","flex");
+    $(".imagenUsuario").css("display","flex");
+    $(".imagenHamburguesa").css("display","flex");
+    $(".imagenBuscador").css("display","flex");
+    $(".logo").css("display","flex");
+    $(".BuscadorDiv").css("display","none");
+    $(".mainContainer").css("filter","brightness(100%)");
+  });
+
+
+  $(".imagenCarrito").on('click', function(){  
+    $(".CarritoDesplegable").css("display","flex");
+    $(".mainContainer").css("filter","brightness(50%)");
+    $(".productsContainer").css("padding-top","10px");
+
+  });
+
+  $(".cerrarDesplegable").on('click', function(){  
+    $(".CarritoDesplegable").css("display","none");
+    $(".mainContainer").css("filter","brightness(100%)");
+    $(".productsContainer").css("padding-top","50px");
+  });
+
+
+
+});
