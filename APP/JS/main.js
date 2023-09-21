7500e5394625f7ad938f8e90dea351323215c5f7
alanmarengo98 <alanmarengo98@gmail.com>
2023-09-21 00:24:54 -0300

    avances hgistorial despletgable

--- APP/JS/main.js
+++ APP/JS/main.js
@@ -87,6 +87,11 @@ $(function() {
     $(".mainContainer").css("filter","brightness(100%)");
   });
 
+
+  $(".imagenUsuario").on('click', function(){  
+    $(".buscadorDesplegable").css("margin-top","70px");
+
+  });
 });
 
 function RenderizarContenidoFooterYHeader(){
@@ -100,7 +105,7 @@ function RenderizarContenidoFooterYHeader(){
 
 const ContenidoHeader = () => 
 `
-  <img type="image" id="logoUsuario" class="imagenUsuario" src="http://127.0.0.1:5500/APP/IMAGES/historial.png" >   
+  <img type="image" id="logoUsuario" class="imagenUsuario" src="http://127.0.0.1:5500/APP/IMAGES/historial.png" >
   <img type="image" id="logoHamburguesa" class="imagenHamburguesa" src="http://127.0.0.1:5500/APP/IMAGES/iconohamburguesa.svg" >
   <img type="imgage" id="checkhamburguesacerrar" class="imagenhamburguesacerrar" src="http://127.0.0.1:5500/APP/IMAGES/CerrarDesplegable.png" >
 
