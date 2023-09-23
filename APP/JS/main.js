a0404d8c66f1fcfb62e5980e6f16d864ff408888
alanmarengo98 <alanmarengo98@gmail.com>
2023-09-23 10:04:02 -0300

    AP-54 SE MEJORA LOGICA DE HEADER CON JS

--- APP/JS/main.js
+++ APP/JS/main.js
@@ -1,15 +1,12 @@
 $(function() {
   RenderizarContenidoFooterYHeader();
 
-  $(".imagenBuscador").on('click', function(){
-    $(".imagenCarrito").css("display","none");
-    $(".imagenUsuario").css("display","none");
-    $(".imagenHamburguesa").css("display","none");
-    $(".imagenBuscador").css("display","none");
-    $(".logo").css("display","none");
-    $(".BuscadorDiv").css("display","grid");
-    $(".header").css("background-color","#5B21A5");
-  });
+  BindBuscador();
+  BindFiltros();
+  BindHistorial();
+  BindCarro();
+  
+
 
   $(".buscador-input").on('keyup', async function(){
     var longidutPalabra = $(this).val().length;
@@ -53,17 +50,10 @@ $(function() {
     }
   });
 
-  $(".imagenCarrito").on('click', function(){  
-    $(".CarritoDesplegable").css("margin-top","70px");
-    $(".mainContainer").css("filter","brightness(50%)");
-    $(".productsContainer").css("padding-top","10px");
-    $(".imagenCarrito").css("display","none");
-    $(".cerrarDesplegable").css("display","flex");
-    RenderizarCarritoEnContenedor();
-
-  });
-
   $(".cerrarDesplegable").on('click', function(){  
+    BindHistorial();
+    BindBuscador();
+    BindFiltros();
     $(".CarritoDesplegable").css("margin-top","-800px");
     $(".mainContainer").css("filter","brightness(100%)");
     $(".productsContainer").css("padding-top","50px");
@@ -71,36 +61,89 @@ $(function() {
     $(".imagenCarrito").css("display","flex");
   });
 
-  $(".imagenHamburguesa").on('click', function(){  
-    $(".filtro").css("margin-top","70px");
-    $(".imagenHamburguesa").css("display","none");
-    $(".imagenhamburguesacerrar").css("display","flex");
-    $(".mainContainer").css("filter","brightness(50%)");
-  });
-
-  $(".imagenhamburguesacerrar").on('click', function(){  
+  $(".imagenhamburguesacerrar").on('click', function(){
+    BindCarro();
+    BindHistorial();
+    BindBuscador();
     $(".imagenhamburguesacerrar").css("display","none");
     $(".imagenHamburguesa").css("display","flex");
     $(".filtro").css("margin-top","-8000px");
     $(".mainContainer").css("filter","brightness(100%)");
   });
 
-
-  $(".imagenUsuario").on('click', function(){  
-    $(".historialDesplegable").css("margin-top","70px");
-    $(".imagenUsuario").css("display","none");
-    $(".cerrarDesplegableHistorial").css("display","block");
-    RenderizarProductosHistorial();
-  });
-
-
-  $(".cerrarDesplegableHistorial").on('click', function(){  
+  $(".cerrarDesplegableHistorial").on('click', function(){
+    BindCarro();
+    BindBuscador();
+    BindFiltros();
     $(".historialDesplegable").css("margin-top","-8000px");
     $(".imagenUsuario").css("display","block");
     $(".cerrarDesplegableHistorial").css("display","none");
   });
 
 });
+
+function EventoImagenCarrito(){
+  $('.imagenUsuario').unbind();
+  $(".imagenBuscador").unbind();
+  $(".imagenHamburguesa").unbind();
+  $(".CarritoDesplegable").css("margin-top","70px");
+  $(".mainContainer").css("filter","brightness(50%)");
+  $(".productsContainer").css("padding-top","10px");
+  $(".imagenCarrito").css("display","none");
+  $(".cerrarDesplegable").css("display","flex");
+  RenderizarCarritoEnContenedor();
+}
+
+function BindCarro(){
+  $(".imagenCarrito").on('click', function(){  
+  $('.imagenUsuario').unbind();
+  $(".imagenBuscador").unbind();
+  $(".imagenHamburguesa").unbind();
+  $(".CarritoDesplegable").css("margin-top","70px");
+  $(".mainContainer").css("filter","brightness(50%)");
+  $(".productsContainer").css("padding-top","10px");
+  $(".imagenCarrito").css("display","none");
+  $(".cerrarDesplegable").css("display","flex");
+  RenderizarCarritoEnContenedor();
+ });
+}
+
+function BindHistorial(){
+  $(".imagenUsuario").on('click', function(){  
+    $(".imagenBuscador").unbind();
+    $(".imagenHamburguesa").unbind();
+    $('.imagenCarrito').unbind();
+    $(".historialDesplegable").css("margin-top","70px");
+    $(".imagenUsuario").css("display","none");
+    $(".cerrarDesplegableHistorial").css("display","block");
+    RenderizarProductosHistorial();
+  });
+
+}
+
+function BindFiltros(){
+  $(".imagenHamburguesa").on('click', function(){  
+  $('.imagenCarrito').unbind();
+  $('.imagenUsuario').unbind();
+  $(".imagenBuscador").unbind();
+  $(".filtro").css("margin-top","70px");
+  $(".imagenHamburguesa").css("display","none");
+  $(".imagenhamburguesacerrar").css("display","flex");
+  $(".mainContainer").css("filter","brightness(50%)");
+  });
+}
+
+function BindBuscador(){
+  $(".imagenBuscador").on('click', function(){
+    $(".imagenCarrito").css("display","none");
+    $(".imagenUsuario").css("display","none");
+    $(".imagenHamburguesa").css("display","none");
+    $(".imagenBuscador").css("display","none");
+    $(".logo").css("display","none");
+    $(".BuscadorDiv").css("display","grid");
+    $(".header").css("background-color","#5B21A5");
+  });
+}
 
 
 
